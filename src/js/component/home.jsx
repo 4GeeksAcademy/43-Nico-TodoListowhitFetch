15ae63239f02b928c7f92abc27ff4db1ce192bd3
Alexander-sys18 <119635571+Alexander-sys18@users.noreply.github.com>
2023-07-12 23:50:01 +0000

    Todo list con fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
 
-import Card from "./card.jsx"
+import Todo from "./todo.jsx"
 
 const Home = () => {
-	async function getCharacters() {
-		try {
-		  const API_URL = "https://rickandmortyapi.com/api";
-		  const requestConfig = {
-			method: "GET",
-			headers: {
-			  "Content-type": "application/json"
-			}
-		  };
-		  const response = await fetch(API_URL + "/character", requestConfig);
-		  if (response.status !== 200) {
-			console.log("Error en la solicitud", response.status);
-			return;
-		  }
-		  const body = await response.json();
-		  console.log(body);
-		} catch (error) {
-		  console.log(error);
-		}
-	  }
-	  
-	  getCharacters();
-	  
+
+	// function Tasks(){
+	// 	try {
+	// 		let response = fetch('https://assets.breatheco.de/apis/fake/todos/user/Nico-43', {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   }});
+	// 	} catch (error) {
+	// 		console.log(error)
+	// 	} finally{
+
+	// 	}
+	// }
+
 	return(
 		<div>
-			<Card />
+			<Todo />
 		</div>
 	);
 };
---
+++ src/js/component/todo.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/Nico-43', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setTodos(data);
+        setTotal(data.length);
+      } else {
+        console.log('Error al obtener los datos');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleInputCambio = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = async (event) => {
+    if (event.key === 'Enter' && inputValue.trim() !== '') {
+      const newTodo = {
+        label: inputValue,
+        done: false
+      };
+
+      try {
+        const response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/Nico-43', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify([...todos, newTodo])
+        });
+
+        if (response.ok) {
+          setTodos([...todos, newTodo]);
+          setInputValue('');
+          setTotal(total + 1);
+        } else {
+          console.log('Error al agregar la tarea');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleBorrarTodo = async (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+
+    try {
+      const response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/Nico-43', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTodos)
+      });
+
+      if (response.ok) {
+        setTodos(newTodos);
+        setTotal(total - 1);
+      } else {
+        console.log('Error al borrar la tarea');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="todo-list-container">
+      <h1 className="todo-list-title">todos</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputCambio}
+        onKeyPress={handleAddTodo}
+        placeholder="Add a new task"
+        className="todo-input"
+      />
+      {todos.length === 0 ? (
+        <p>No hay tareas</p>
+      ) : (
+        <ul className="todo-items">
+          {todos.map((todo, index) => (
+            <li key={index} className="todo-item">
+              {todo.label}
+              <button onClick={() => handleBorrarTodo(index)} className="delete-button">
+                <i className="fa-solid fa-trash"></i>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className="total">
+        {total} {total === 1 ? 'item left' : 'items left'}
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
+
