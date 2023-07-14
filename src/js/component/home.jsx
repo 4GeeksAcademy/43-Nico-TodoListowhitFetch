4b074b0ffb7fe0a969594701968cc04474ce736e
Alexander-sys18 <119635571+Alexander-sys18@users.noreply.github.com>
2023-07-14 22:47:49 +0000

    tercer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,21 +3,6 @@ import React from 'react';
 import Todo from "./todo.jsx"
 
 const Home = () => {
-
-	// function Tasks(){
-	// 	try {
-	// 		let response = fetch('https://assets.breatheco.de/apis/fake/todos/user/Nico-43', {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }});
-	// 	} catch (error) {
-	// 		console.log(error)
-	// 	} finally{
-
-	// 	}
-	// }
-
 	return(
 		<div>
 			<Todo />
